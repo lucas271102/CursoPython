d85f6c80ab9cc4e1423b496c8ec289fcd8105f64
lucas271102 <lucas001spe@gmail.com>
2024-07-20 14:55:50 -0300

    datos

--- tiposdedatos/datos-compuestos.py
+++ tiposdedatos/datos-compuestos.py
@@ -7,3 +7,12 @@ tupla=("Lucas Speziale", "Crystal Mountain", "Lift operator")
 
 #creando un conjunto (set)
 conjunto = {"Lucas Speziale", "Crystal Mountain", "Lift operator"}
+
+#creando un diccionario (dict)
+
+diccionario = {
+    'nombre': 'Lucas',
+    'apellido': 'Speziale',
+    'edad': '21'
+}
+print(diccionario['nombre'])
