eccba2296ef6e4850eab7833d34bdeab3a0658de
lucas271102 <lucas001spe@gmail.com>
2024-07-20 01:23:51 -0300

    python

---
+++ tiposdedatos/datos-compuestos.py
@@ -0,0 +1,9 @@
+
+
+#las listas se pueden modificar
+lista = ["Lucas Speziale", "Crystal Mountain", "Lift operator"]
+#las tuplas no se pueden modificar 
+tupla=("Lucas Speziale", "Crystal Mountain", "Lift operator")
+
+#creando un conjunto (set)
+conjunto = {"Lucas Speziale", "Crystal Mountain", "Lift operator"}
