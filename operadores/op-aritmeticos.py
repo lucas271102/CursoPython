2b692b605acbd208cd057079ec368e962f4c108b
lucas271102 <lucas001spe@gmail.com>
2024-07-23 21:49:41 -0300

    opComparacion

---
+++ operadores/op-aritmeticos.py
@@ -0,0 +1,22 @@
+
+#suma y resta 
+suma = 12 + 5  
+resta = 12 - 5
+
+
+#multiplicacion y division
+multiplicacion = 12 * 5
+division = 12 / 5 
+
+
+#potenciación (**)
+
+exponente = 12 ** 5 
+
+#division baja //
+division_baja = 12 // 5
+
+#resto o modulo 
+resto = 12 % 5 
+
+print (division_baja)
