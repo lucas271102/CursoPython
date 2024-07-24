2a7204e295a1a2d537cff64da90a538a301ce5a0
lucas271102 <lucas001spe@gmail.com>
2024-07-24 11:16:21 -0300

    operadoresLogicos

---
+++ operadores/op-logicos.py
@@ -0,0 +1,19 @@
+#AND
+Resultado1 = True & True #Devolver True
+Resultado2 = False & True #Devolver falso
+Resultado3 = True & False #Devolver falso
+Resultado4 = False & False #Devolver falso
+
+#OR
+Resultado5= True | True #Devolver True
+Resultado6 = False | True #Devolver True
+Resultado7 = True | False #Devolver True
+Resultado8 = False | False #Devolver falso
+
+
+
+#NOT
+Resultado9 =  not 27 > 21  #Devolver falso
+Resultado10 = not  False  #Devolver True
+
+print ( Resultado9)
