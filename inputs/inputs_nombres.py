1275ef9106c21a4e0da1967a946f7aba02d8eb73
lucas271102 <lucas001spe@gmail.com>
2024-07-30 12:14:29 -0300

    inputs

---
+++ inputs/inputs_nombres.py
@@ -0,0 +1,5 @@
+#pedrile un dato al usuario
+nombre = input ("Cual es tu nombre?")
+#el dato que devuelven los inputs siempre es un texto 
+#mostrando el dato 
+print (f'el nombre es:{nombre}')
---
+++ inputs/inputs_numeros.py
@@ -0,0 +1,11 @@
+#pedirle u numero al usuario 
+numero = input("Dame un numero")
+
+#convierto el numero a entero y lo multiplico por 2 
+resultado = int(numero) * 2 
+
+#convierto el numero a flotante  y lo multiplico por 2 
+resultado2 = float(numero) * 2 
+
+#mostrando el resultado 
+print(resultado2)
