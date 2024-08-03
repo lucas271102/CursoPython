5b43ee5577464d0ba1074da29adf0ce8ff02dfa3
lucas271102 <lucas001spe@gmail.com>
2024-08-03 12:00:01 -0300

    fin_bucles

---
+++ bucles/bucle_while.py
@@ -0,0 +1,8 @@
+contador = 0 
+
+#para que se ejecute tiene que cumplir la condicion, sino, se detiene (vuelta tras vuekta se verifica la condicion)
+while contador < 19 :
+    print(contador)
+    contador += 1 
+    
+print("llegó a su fin")
