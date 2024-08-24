9c37e7780ec852d92597cda4136483b6774c468f
lucas271102 <lucas001spe@gmail.com>
2024-08-23 22:14:57 -0300

    sorrt

--- archivos/leer_csv_con_pandas.py
+++ archivos/leer_csv_con_pandas.py
@@ -2,5 +2,17 @@ import pandas as pd
 #df = dataframe
 #usando la funcion read_csv para leer el archivo csv
 
-df = pd.read_csv("archivos//datos.csv")
-print(df["nombre"]) 
+df = pd.read_csv("archivos//datos.csv", names=["name", "lastname", "age"])
+nombres = df["name"]
+#utilizando metodo slicing para hayar caracteres de un string 
+
+cadena = "0123456789"
+
+#ordenando el dataframe por edad
+df_ordenado = df.sort_values("age")
+
+#ordenando de forma descendente
+df_ordenado_al_reves = df.sort_values("age", ascending=False)
+print(df_ordenado_al_reves)
+#print(df_ordenado)
+#print(cadena[0:5])
