05dd7ec8c191c5017bb76daf45be083396d143a0
lucas271102 <lucas001spe@gmail.com>
2024-09-05 15:30:44 -0300

    archivos/CSV

--- archivos/leer_csv_con_pandas.py
+++ archivos/leer_csv_con_pandas.py
@@ -14,7 +14,7 @@ nombres = df["nombre"]
 #cadena = "0123456789"
 
 #ordenando el dataframe por edad
-#df_ordenado = df.sort_values("edad")
+df_ordenado = df.sort_values("edad")
 
 #ordenando de forma descendente
 #df_ordenado_al_reves = df.sort_values("edad", ascending=False)
@@ -37,8 +37,27 @@ columnas_totales = filas_columnas[1]
 #obteniemdo datos estadisticos del dataframe
 df_stats = df.describe()
 
+#accediendo a la edad de la fila 2 con iloc
 
+elm_esp = df.iloc[0,2]
+
+#accediendo a todas las filas de una columna
+apellidos = df.iloc[:, 1]
+
+#accediemdo a las edades
+edades = df.iloc[:, 2]
+
+#accediendo a la fila 3
+fila3 = df.loc[2, :]
+
+#accediendo a filas con edad mayor que 30 
+mayores_30 = df.loc[df["edad"]>45, :]
+
+#print(mayores_30)
+#print(elm_esp)
+#print(apellidos, edades)
+#print(fila3)
 print(df_stats)
 #print(df_concatenado)
-#print(df_ordenado)
+print(df_ordenado)
 #print(cadena[0:5])
