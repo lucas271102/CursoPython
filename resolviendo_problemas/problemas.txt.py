a7a7c0df558e68e8773e037c76b40d21589e7bfe
lucas271102 <lucas001spe@gmail.com>
2024-09-08 20:04:58 -0300

    problemasTxt

---
+++ resolviendo_problemas/problemas.txt.py
@@ -0,0 +1,11 @@
+#2 listas, una con nombres otra con apellidos 
+nombres = ["Lucas", "Matias", "Camila", "Pedro"]
+apellidos = [ "Dalto", "Zing", "Dalto", "Robertix"]
+
+#Registrar esta informacion en un TXT de forma optima 
+
+with open ("/Users/lucasspeziale/Desktop/Curso de Python/resolviendo_problemas/nombres_y_apellidos.txt", "w") as arch:
+    arch.writelines("Los datos son:\n")
+    [arch.writelines(f"Nombre: {n}\nApellido:{a}\n---------\n")for n,a in zip (nombres, apellidos)]
+
+
