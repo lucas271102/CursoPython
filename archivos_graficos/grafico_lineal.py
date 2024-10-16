ef5dfe366f8484bc1c1bebfb95bb969619f39aac
lucas271102 <lucas001spe@gmail.com>
2024-10-15 23:04:50 -0300

    graficos

--- archivos_graficos/grafico_lineal.py
+++ archivos_graficos/grafico_lineal.py
@@ -1,10 +1,13 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns 
+from flask import Flask, send_file
 
 dfv = pd.read_csv("archivos_graficos//ventas.csv")
 print(dfv)
 
 #sns.lineplot(x="Mes", y="Ventas", data=dfv )
 sns.barplot(x="Mes", y="Ventas", data=dfv )
+plt.title('Informe de ventas del año 2023')
+plt.savefig('archivos_graficos/grafico.png')
 plt.show()
--- archivos_graficos/ventas.py
+++ archivos_graficos/ventas.py
@@ -4,7 +4,7 @@ meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto
 ventas = [150, 200, 220, 240, 280, 300, 450, 320, 600, 250, 900, 290]
 
 #escribir en un nuevo archivo csv 
-with open ('ventas.csv', 'w', newline='') as archivo_csv:
+with open ('archivos_graficos/ventas.csv', 'w', newline='') as archivo_csv:
     writer = csv.writer(archivo_csv)
     writer.writerow(['Mes', 'Ventas'])
     for mes, venta in zip(meses, ventas):
