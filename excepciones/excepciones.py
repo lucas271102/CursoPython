12eec0422be49761261322c96aa048584a61aca9
lucas271102 <lucas001spe@gmail.com>
2024-12-05 12:02:44 -0300

    excepciones

--- excepciones/excepciones.py
+++ excepciones/excepciones.py
@@ -1,7 +1,10 @@
 def sumar_dos():
     a = input("Numero 1:")
     b = input("Numero 2:")
-    resultado = int (a) +int(b)
+    try:
+        resultado = int (a) +int(b)
+    except:
+        print("No te detengas ")
     return resultado
 
 print(sumar_dos())
